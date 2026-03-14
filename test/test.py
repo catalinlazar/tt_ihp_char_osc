9f026b3d322ed31a8a916060e61a3012c711f96e
Catalin Lazar <catalin.lazar.subs@gmail.com>
2026-03-14 20:58:39 +0100

    test

--- test/test.py
+++ test/test.py
@@ -1,17 +1,13 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
-
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
 
-
 @cocotb.test()
 async def test_project(dut):
     dut._log.info("Start")
-
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
+    
+    # Set the clock period to 100ns (10 MHz)
+    clock = Clock(dut.clk, 100, units="ns")
     cocotb.start_soon(clock.start())
 
     # Reset
@@ -23,18 +19,11 @@ async def test_project(dut):
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
+    dut._log.info("Test Enable and Byte Select")
+    # Enable the oscillator (ui_in[1])
+    dut.ui_in.value = 2 
+    
+    # Wait for the 10ms window (we can't wait 10ms in sim, so we just wait some cycles)
+    await ClockCycles(dut.clk, 100)
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
-
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    dut._log.info("Finished Test")
