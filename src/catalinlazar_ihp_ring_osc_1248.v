be884f7c9ad41cbc90d81fe2bb86e63fe5ce79c0
Catalin Lazar <catalin.lazar.subs@gmail.com>
2026-03-16 22:57:14 +0100

    cell_reference

--- src/catalinlazar_ihp_ring_osc_1248.v
+++ src/catalinlazar_ihp_ring_osc_1248.v
@@ -11,15 +11,20 @@ module catalinlazar_ihp_ring_osc_1248 #(
     (* keep *) sg13g2_stdcell_nand2_1 gate0 (.A(en), .B(nodes[STAGES]), .Y(nodes[0]));
 
     // Generate the long chain
-    genvar i;
-    generate
-        for (i = 1; i <= STAGES; i = i + 1) begin : oscloop
-            if (DRIVE == 1)      (* keep *) sg13g2_stdcell_inv_1 inv (.A(nodes[i-1]), .Y(nodes[i]));
-            else if (DRIVE == 2) (* keep *) sg13g2_stdcell_inv_2 inv (.A(nodes[i-1]), .Y(nodes[i]));
-            else if (DRIVE == 4) (* keep *) sg13g2_stdcell_inv_4 inv (.A(nodes[i-1]), .Y(nodes[i]));
-            else                 (* keep *) sg13g2_stdcell_inv_8 inv (.A(nodes[i-1]), .Y(nodes[i]));
-        end
-    endgenerate
-
-    assign clk_out = nodes[STAGES];
+   genvar i;
+   generate
+      for (i = 1; i <= STAGES; i = i + 1) begin : oscloop
+         if (DRIVE == 1) begin : gen_drv1
+            (* keep *) sg13g2_stdcell_inv_1 inv (.A(nodes[i-1]), .Y(nodes[i]));
+         end else if (DRIVE == 2) begin : gen_drv2
+            (* keep *) sg13g2_stdcell_inv_2 inv (.A(nodes[i-1]), .Y(nodes[i]));
+         end else if (DRIVE == 4) begin : gen_drv4
+            (* keep *) sg13g2_stdcell_inv_4 inv (.A(nodes[i-1]), .Y(nodes[i]));
+         end else begin : gen_drv8
+            (* keep *) sg13g2_stdcell_inv_8 inv (.A(nodes[i-1]), .Y(nodes[i]));
+         end
+      end
+   endgenerate
+   
+   assign clk_out = nodes[STAGES];
 endmodule
