a42bcfd96a45c7b0c8c6c4bef09f1242a83d4ff7
Catalin Lazar <catalin.lazar.subs@gmail.com>
2026-03-19 16:05:20 +0100

    display

--- src/tt_um_catalinlazar_ihp_osc_array.v
+++ src/tt_um_catalinlazar_ihp_osc_array.v
@@ -79,9 +79,9 @@ module tt_um_catalinlazar_ihp_osc_array (
       end
    end
    
-   always @(posedge selected_hsc) begin
-      $display("OSC EDGE at time %t, count now %d", $time, count);
-   end
+#   always @(posedge selected_hsc) begin
+#      $display("OSC EDGE at time %t, count now %d", $time, count);
+#   end
    
    assign uo_out = (b_sel_reg == 2'b00) ? snapshot[7:0]   :
                     (b_sel_reg == 2'b01) ? snapshot[15:8]  :
